957b66a2406a3accc8dcac0fcde4c7620ecbe836
Thomas Abraham <thomasabraham@users.noreply.github.com>
2019-12-17 07:21:21 +0100

    Initialized a Kotlin CLI application

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,3 +4,36 @@
  * This is a general purpose Gradle build.
  * Learn how to create Gradle builds at https://guides.gradle.org/creating-new-gradle-builds
  */
+
+plugins {
+    // Apply the Kotlin JVM plugin to add support for Kotlin.
+    id("org.jetbrains.kotlin.jvm") version "1.3.41"
+
+    // Apply the application plugin to add support for building a CLI application.
+    application
+}
+
+repositories {
+    // Use jcenter for resolving dependencies.
+    // You can declare any Maven/Ivy/file repository here.
+    jcenter()
+}
+
+dependencies {
+    // Align versions of all Kotlin components
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+
+    // Use the Kotlin JDK 8 standard library.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Use the Kotlin test library.
+    testImplementation("org.jetbrains.kotlin:kotlin-test")
+
+    // Use the Kotlin JUnit integration.
+    testImplementation("org.jetbrains.kotlin:kotlin-test-junit")
+}
+
+application {
+    // Define the main class for the application
+    mainClassName = "com.ahmost.template.AppKt"
+}
